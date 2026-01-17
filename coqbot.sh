3f6b35b560711b647991bd6acd4394b10aea1893
coqbot <coqbot@users.noreply.github.com>
2026-01-17 21:02:59 +0000

    Added user script in coqbot.sh for SkySkimmer in rocq-prover/rocq
    Comment Thread ID: I_kwDOABUDh87kCJyS

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,5 @@
-opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+#!/usr/bin/env bash
+cat > thebug.v <<'EOF'
+e https://gricad-gitlab.univ-grenoble-alpes.fr/certicompil/Chamois-CompCert.git -b chamois-merge-btlmemcpy
+EOF
+coqc -q thebug.v
