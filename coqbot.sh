61ef60d4ad6cb86a3dfdcd886f28becf1cf4750b
coqbot <coqbot@users.noreply.github.com>
2026-06-15 04:14:34 +0000

    Added user script in coqbot.sh for JasonGross in rocq-prover/rocq
    Comment Thread ID: I_kwDOABUDh88AAAABFY4dmw

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,6 @@
-opam update -y
-opam install -y coq-ext-lib
+#!/usr/bin/env bash
+opam install -y coq-coqprime
+echo 'Require Import Coqprime.PrimalityTest.Root.' > foo.v
 eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+rocq c foo.v
+rocq check foo
